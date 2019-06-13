22d348c29da70ef5cc6d2c26e0dc2c722c3c10ec
Michiel Valee <michiel@dockflow.be>
2019-06-13 19:07:10 +0200

    socket data

--- src/services/socket-reader-service.ts
+++ src/services/socket-reader-service.ts
@@ -23,8 +23,10 @@ export class SocketReaderService{
         this.io.on('chaincodeevent', function(data:any){
             if(data && data.event){
                 //self.aHandlerObject.handleData(data.event);
-                logger.debug('A chaincode event happened.');
-                logger.debug("txId: " + data.tx_id + ": " + data.payload.schema + " / "+ data.payload.id.substr(0,36) +" )");
+                logger.debug('A chaincode event happened.' + (data.tx_id ? data.tx_id : '-no tx id-'));
+                if(data.payload && data.payload.schema && data.payload.id){
+                     logger.debug("txId: " + data.tx_id + ": " + data.payload.schema + " / "+ data.payload.id.substr(0,36) +" )");
+                }
             }
             
         });
