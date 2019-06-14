691f6b1d150f3451360c46cf05a2660e71533e0d
Michiel Valee <michiel@dockflow.be>
2019-06-14 10:01:12 +0200

    dint ignore

---
+++ rebase.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+git checkout philipverschueren
+git rebase master
+git push
+yannickspillemaeckers
+git rebase master
+git push
+philiphuysmans
+git rebase master
+git push
+vibhutimantri
+git rebase master
+git push
+ronnystormezand
+git rebase master
+git push
+lukariester
+git rebase master
+git push
+henrigroenweghe
+git rebase master
+git push
+lievevereycken
+git rebase master
+git push
+participantnine
+git rebase master
+git push
+participantten
+git rebase master
+git push
+participanteleven
+git rebase master
+git push
+participanttwelve
+git rebase master
+git push
+participantthirtheen
+git rebase master
+git push
+participantfourteen
+git rebase master
+git push
+participantfifteen
+git rebase master
+git push
