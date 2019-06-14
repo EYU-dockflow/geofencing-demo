a8158dd292632e4fc710d1827e710aac9feb92ee
Michiel Valee <michiel@dockflow.be>
2019-06-14 11:40:07 +0200

    geotools

--- src/geofence.ts
+++ src/geofence.ts
@@ -2,6 +2,7 @@ import { SocketReaderService } from "./services/socket-reader-service";
 import { logger } from "./services/logger";
 
 import { Point } from 'geojson'
+var geotools = require('geojson-tools');
 
 export class Geofence{
 
@@ -13,6 +14,8 @@ export class Geofence{
         socket.on('transaction',(transaction, object)=>{
             logger.debug('transaction:'  + JSON.stringify(transaction));
             logger.debug('object' + JSON.stringify(object));
+
+            //geotools.getDistance([]);
             
         })
     }
